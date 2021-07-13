bd9edfd86a3205d5f44fc8a8dc762d586a065538
lenewe <linewwei@126.com>
2021-07-13 08:34:29 +0800

    添加项目文件。

---
+++ Sinks/Batch/PeriodicCleanup.cs
@@ -0,0 +1,82 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace Serilog.Sinks.MySQL.Sinks.Batch
+{
+    internal class PeriodicCleanup
+    {
+        private readonly string _connectionString;
+        private readonly string _tableName;
+        private readonly string _columnNameWithTime;
+        private readonly TimeSpan _recordsExpiration;
+        private readonly TimeSpan _cleanupFrequency;
+        private readonly bool _timeInUtc;
+        private Timer _cleanupTimer;
+        private readonly int _deleteLimit;
+
+        /// <summary>
+        /// 清理日志
+        /// </summary>
+        /// <param name="connectionString">数据库连接字符串</param>
+        /// <param name="tableName">删除的表名称</param>
+        /// <param name="columnNameWithTime">表中时间字段名称</param>
+        /// <param name="recordsExpiration">过期记录TimeSpan</param>
+        /// <param name="cleanupFrequency">记录清理频率</param>
+        /// <param name="timeInUtc">时间戳InUtc</param>
+        /// <param name="deleteLimit">一次删除多少个</param>
+        public PeriodicCleanup(string connectionString, string tableName, string columnNameWithTime, TimeSpan? recordsExpiration, TimeSpan? cleanupFrequency, bool timeInUtc, int deleteLimit)
+        {
+            if (string.IsNullOrEmpty(columnNameWithTime))
+                throw new ArgumentNullException(nameof(columnNameWithTime));
+            if (_deleteLimit < 0)
+                throw new ArgumentOutOfRangeException(nameof(deleteLimit));
+
+            _columnNameWithTime = columnNameWithTime;
+            _connectionString = connectionString;
+            _tableName = tableName;
+            _recordsExpiration = recordsExpiration.Value;
+            _cleanupFrequency = cleanupFrequency.Value;
+            _timeInUtc = timeInUtc;
+            _deleteLimit = deleteLimit;
+        }
+
+        public void Start()
+        {
+            //We are delaying first cleanup for 2 seconds just to avoid semi-expensive query at startup
+            _cleanupTimer = new Timer(EnsureCleanup, null, 2000, (int)_cleanupFrequency.TotalMilliseconds);
+        }
+
+        private void EnsureCleanup(object state)
+        {
+            try
+            {
+                using (var conn = new MySqlConnection(_connectionString))
+                {
+                    conn.Open();
+                    int affectedRows = 0;
+                    do
+                    {
+                        var sql = $"DELETE FROM `{_tableName}` WHERE `{_columnNameWithTime}` < @expiration LIMIT {_deleteLimit}";
+                        using (var cmd = new MySqlCommand(sql, conn))
+                        {
+                            var deleteFromTime = _timeInUtc
+                                ? DateTimeOffset.UtcNow - _recordsExpiration
+                                : DateTimeOffset.Now - _recordsExpiration;
+
+                            cmd.Parameters.AddWithValue("expiration", deleteFromTime);
+                            affectedRows = cmd.ExecuteNonQuery();
+                        }
+                    } while (affectedRows >= _deleteLimit && affectedRows > 1);
+                }
+            }
+            catch (Exception ex)
+            {
+                Serilog.Debugging.SelfLog.WriteLine("Periodic database cleanup failed: " + ex.ToString());
+            }
+        }
+
+    }
+}
---
+++ Sinks/MySQL/MySqlSink.cs
@@ -0,0 +1,197 @@
+﻿// Copyright 2019 Zethian Inc.
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+using Serilog.Core;
+using Serilog.Debugging;
+using Serilog.Events;
+using Serilog.Sinks.Batch;
+using Serilog.Sinks.Extensions;
+using Serilog.Sinks.MySQL.Sinks.Batch;
+
+namespace Serilog.Sinks.MySQL
+{
+    public class MySqlSink : BatchProvider, ILogEventSink
+    {
+        private readonly string _connectionString;
+        private readonly bool _storeTimestampInUtc;
+        private readonly string _tableName;
+        private readonly PeriodicCleanup _cleaner;
+
+        public MySqlSink(
+            string connectionString,
+             TimeSpan? recordsExpiration, TimeSpan? cleanupFrequency, string columnNameWithTime, int deleteLimit, string tableName, uint batchSize, bool storeTimestampInUtc) : base((int)batchSize)
+        {
+            _connectionString = connectionString;
+            _tableName = tableName;
+            _storeTimestampInUtc = storeTimestampInUtc;
+
+            var sqlConnection = GetSqlConnection();
+            CreateTable(sqlConnection);
+
+            if (recordsExpiration.HasValue && recordsExpiration > TimeSpan.Zero && cleanupFrequency.HasValue && cleanupFrequency > TimeSpan.Zero)
+            {
+                _cleaner = new PeriodicCleanup(connectionString,
+                    tableName,
+                    columnNameWithTime,
+                    recordsExpiration,
+                    cleanupFrequency,
+                    storeTimestampInUtc,
+                    deleteLimit);
+                _cleaner.Start();
+            }
+        }
+
+        public void Emit(LogEvent logEvent)
+        {
+            PushEvent(logEvent);
+        }
+
+        private MySqlConnection GetSqlConnection()
+        {
+            try
+            {
+                var conn = new MySqlConnection(_connectionString);
+                conn.Open();
+
+                return conn;
+            }
+            catch (Exception ex)
+            {
+                SelfLog.WriteLine(ex.Message);
+
+                return null;
+            }
+        }
+
+        private MySqlCommand GetInsertCommand(MySqlConnection sqlConnection)
+        {
+            var tableCommandBuilder = new StringBuilder();
+            tableCommandBuilder.Append($"INSERT INTO  {_tableName} (");
+            tableCommandBuilder.Append("Timestamp, Level, Message, LongDate,Logger,TraceIdentifier ,Exception, Properties) ");
+            tableCommandBuilder.Append("VALUES (@ts, @level, @msg,@LongDate,@Logger,@TraceIdentifier, @ex, @prop)");
+
+            var cmd = sqlConnection.CreateCommand();
+            cmd.CommandText = tableCommandBuilder.ToString();
+
+            cmd.Parameters.Add(new MySqlParameter("@ts", MySqlDbType.VarChar));
+            cmd.Parameters.Add(new MySqlParameter("@level", MySqlDbType.VarChar));
+            cmd.Parameters.Add(new MySqlParameter("@msg", MySqlDbType.VarChar));
+            cmd.Parameters.Add(new MySqlParameter("@LongDate", MySqlDbType.DateTime));
+            cmd.Parameters.Add(new MySqlParameter("@Logger", MySqlDbType.VarChar));
+            cmd.Parameters.Add(new MySqlParameter("@TraceIdentifier", MySqlDbType.VarChar));
+            cmd.Parameters.Add(new MySqlParameter("@ex", MySqlDbType.VarChar));
+            cmd.Parameters.Add(new MySqlParameter("@prop", MySqlDbType.VarChar));
+
+            return cmd;
+        }
+
+        private void CreateTable(MySqlConnection sqlConnection)
+        {
+            try
+            {
+                var tableCommandBuilder = new StringBuilder();
+                tableCommandBuilder.Append($"CREATE TABLE IF NOT EXISTS {_tableName} (");
+                tableCommandBuilder.Append("id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,");
+                tableCommandBuilder.Append("Timestamp VARCHAR(100),");
+                tableCommandBuilder.Append("Level VARCHAR(15),");
+                tableCommandBuilder.Append("Message VARCHAR(2048),");
+                tableCommandBuilder.Append("LongDate datetime DEFAULT NULL,");
+                tableCommandBuilder.Append("Logger varchar(128) DEFAULT NULL,");
+                tableCommandBuilder.Append("TraceIdentifier varchar(128) DEFAULT NULL,");
+                tableCommandBuilder.Append("Exception varchar(1024),");
+                tableCommandBuilder.Append("Properties varchar(1024),");
+                tableCommandBuilder.Append("_ts TIMESTAMP DEFAULT CURRENT_TIMESTAMP)");
+
+                var cmd = sqlConnection.CreateCommand();
+                cmd.CommandText = tableCommandBuilder.ToString();
+                cmd.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                SelfLog.WriteLine(ex.Message);
+            }
+        }
+        protected override async Task<bool> WriteLogEventAsync(ICollection<LogEvent> logEventsBatch)
+        {
+            try
+            {
+                using (var sqlCon = GetSqlConnection())
+                {
+                    using (var tr = await sqlCon.BeginTransactionAsync().ConfigureAwait(false))
+                    {
+                        var insertCommand = GetInsertCommand(sqlCon);
+                        insertCommand.Transaction = tr;
+                        foreach (var logEvent in logEventsBatch)
+                        {
+                            var logMessageString = new StringWriter(new StringBuilder());
+                            logEvent.RenderMessage(logMessageString);
+
+                            insertCommand.Parameters["@ts"].Value = _storeTimestampInUtc
+                                ? logEvent.Timestamp.ToUniversalTime().ToString("yyyy-MM-dd HH:mm:ss.fffzzz")
+                                : logEvent.Timestamp.ToString("yyyy-MM-dd HH:mm:ss.fffzzz");
+
+                            insertCommand.Parameters["@level"].Value = logEvent.Level.ToString();
+                            insertCommand.Parameters["@msg"].Value = logMessageString;
+                            insertCommand.Parameters["@LongDate"].Value = _storeTimestampInUtc
+                                ? logEvent.Timestamp.ToUniversalTime().ToString("yyyy-MM-dd HH:mm:ss")
+                                : logEvent.Timestamp.ToString("yyyy-MM-dd HH:mm:ss");
+                            logEvent.Properties.TryGetValue("SourceContext", out LogEventPropertyValue sourceContext);
+
+                            logEvent.Properties.TryGetValue("\"Message\"", out LogEventPropertyValue tagMessage);
+                            if (tagMessage != null)
+                            {
+                                var tagObject = tagMessage as ScalarValue;
+                                if (tagObject?.Value.ToString().Contains("logTag") == true)
+                                {
+                                    var obj = tagObject.Value.ToString().Replace("=", "").Replace("{", "")
+                                        .Replace("}", "").Replace("logTag", "").Trim();
+                                    insertCommand.Parameters["@Logger"].Value =
+                                        obj ?? string.Empty;
+                                }
+                            }
+                            else
+                            {
+                                insertCommand.Parameters["@Logger"].Value = (sourceContext as ScalarValue)?.Value ?? string.Empty;
+                            }
+                            logEvent.Properties.TryGetValue("RequestId", out LogEventPropertyValue requestId);
+                            insertCommand.Parameters["@TraceIdentifier"].Value = (requestId as ScalarValue)?.Value ?? string.Empty;
+                            insertCommand.Parameters["@ex"].Value = logEvent.Exception?.ToString() ?? string.Empty;
+                            insertCommand.Parameters["@prop"].Value = logEvent.Properties.Count > 0
+                                ? logEvent.Properties.Json()
+                                : string.Empty;
+
+                            await insertCommand.ExecuteNonQueryAsync().ConfigureAwait(false);
+                        }
+
+                        tr.Commit();
+
+                        return true;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                SelfLog.WriteLine(ex.Message);
+
+                return false;
+            }
+        }
+    }
+}
